e904d5b4397aaf8701841343a4f972d0162e7a96
Swdenis <swdenis@mail.ru>
2021-02-07 12:25:42 +0100

    Added memoization and list of posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,22 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () => async dispatch => {
     const res = await jsonPlaceholder.get('/posts');
 
     dispatch({type: 'FETCH_POSTS', payload: res.data})
 }
+
+export const fetchUser = id => dispatch => _fetchUser(id,dispatch)
+const _fetchUser = _.memoize(async (id,dispatch) => {
+    const res = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({type: 'FETCH_USER', payload: res.data})
+})
+
+// export const fetchUser = function(id){
+//     return async function(dispatch) {
+//     const res = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({type: 'FETCH_USER', payload: res.data})
+// }}
